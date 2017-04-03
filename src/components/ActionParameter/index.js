9a1a6d1c00d93c9acad3dafbcd8d6131239e9683
Paul-Guillaume Déjardin <pgdejardin@xebia.fr>
2017-04-03 17:56:53 +0200

    Parameters for Action

--- src/components/ActionParameter/index.js
+++ src/components/ActionParameter/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import './styles.css';
 
-export default function ActionParameter({parameter}) {
+export default function ActionParameter({ parameter }) {
   return (
     <div className="ActionParameter-main">
-      <div className="ActionParameter-name">{parameter.name}</div>
-      <code className="ActionParameter-type">{parameter.type}</code>
+      <div className="ActionParameter-name">{parameter.name}</div>{' '}
+      <code className="ActionParameter-type">{parameter.type}</code>{' '}
+      {parameter.required && <span className="ActionParameter-required">(requis){' '}</span>}
+      <span className="text-muted ActionParameter-example">
+        <strong>Exemple:{' '}</strong>
+        <span>{parameter.example}</span>
+      </span>
     </div>
   );
 }
@@ -15,5 +20,7 @@ ActionParameter.propTypes = {
   parameter: React.PropTypes.shape({
     name: React.PropTypes.string,
     type: React.PropTypes.string,
+    required: React.PropTypes.bool,
+    example: React.PropTypes.string,
   }),
 };
